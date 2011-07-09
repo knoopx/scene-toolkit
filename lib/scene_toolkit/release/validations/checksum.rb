2e9f44da575bbac450526f6944f8e8d02af343e5
Víctor Martínez <knoopx@gmail.com>
2011-07-09 02:33:29 +0200

    Rescue SFV I/O read exceptions.

--- lib/scene_toolkit/release/validations/checksum.rb
+++ lib/scene_toolkit/release/validations/checksum.rb
@@ -10,43 +10,52 @@ module SceneToolkit
 
         def valid_checksum?
           @errors[:checksum], @warnings[:checksum] = [], []
-
           if sfv_files.any?
             sfv_files.each do |sfv|
-              files_to_check = files.inject({ }) do |collection, file|
-                collection[File.basename(file).downcase] = File.expand_path(file)
-                collection
+              begin
+                validate_checksum(sfv)
+              rescue => e
+                @errors[:playlist] << e.message
               end
-
-              matched_something = false
-              File.read(sfv).split(/[\r\n]+/).each do |line|
-                line.strip!
-
-                if (/(generated|raped)/i =~ line and not /MorGoTH/i =~ line)
-                  @warnings[:checksum] << "Possibly tampered SFV: #{line.strip}"
-                end
-
-                if match = /^(.+?)\s+([\dA-Fa-f]{8})$/.match(line)
-                  matched_something = true
-                  filename, checksum = match.captures
-                  filename.strip!
-                  filename.downcase!
-
-                  if files_to_check.has_key?(filename)
-                    unless Zlib.crc32(File.read(files_to_check[filename])).eql?(checksum.hex)
-                      @errors[:checksum] << "File #{filename} is corrupted"
-                    end
-                  else
-                    @errors[:checksum] << "File #{filename} not found"
-                  end
-                end
-              end
-              @warnings[:checksum] << "No files to verify found" unless matched_something
             end
           else
             @errors[:checksum] << "No *.sfv files found"
           end
           @errors[:checksum].empty?
+        end
+
+        protected
+
+        def validate_checksum(sfv)
+          files_to_check = files.inject({ }) do |collection, file|
+            collection[File.basename(file).downcase] = File.expand_path(file)
+            collection
+          end
+
+          matched_something = false
+          File.read(sfv).split(/[\r\n]+/).each do |line|
+            line.strip!
+
+            if (/(generated|raped)/i =~ line and not /MorGoTH/i =~ line)
+              @warnings[:checksum] << "Possibly tampered SFV: #{line.strip}"
+            end
+
+            if match = /^(.+?)\s+([\dA-Fa-f]{8})$/.match(line)
+              matched_something = true
+              filename, checksum = match.captures
+              filename.strip!
+              filename.downcase!
+
+              if files_to_check.has_key?(filename)
+                unless Zlib.crc32(File.read(files_to_check[filename])).eql?(checksum.hex)
+                  @errors[:checksum] << "File #{filename} is corrupted"
+                end
+              else
+                @errors[:checksum] << "File #{filename} not found"
+              end
+            end
+          end
+          @warnings[:checksum] << "No files to verify found" unless matched_something
         end
       end
     end
