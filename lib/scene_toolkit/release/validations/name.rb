df4953592bc6119e0cc9e32b09a3b814d592b9fa
Víctor Martínez <knoopx@gmail.com>
2011-07-10 16:26:12 +0200

    Constantized release name regexp

--- lib/scene_toolkit/release/validations/name.rb
+++ lib/scene_toolkit/release/validations/name.rb
@@ -2,6 +2,8 @@ module SceneToolkit
   class Release
     module Validations
       module Name
+        REGEXP = /^([A-Z0-9\-_.()&]+)\-(\d{4}|\d{3}x|\d{2}xx)\-([A-Z0-9_]+)$/i
+
         def self.included(base)
           base.register_validation(:name, "Validate release name")
         end
@@ -9,7 +11,7 @@ module SceneToolkit
         def valid_name?(params = {})
           @errors[:name], @warnings[:name] = [], []
 
-          @errors[:name] << "Release name is not a valid scene release name" unless @name =~ /^([A-Z0-9\-_.()&]+)\-(\d{4}|\d{3}x|\d{2}xx)\-([A-Z0-9_]+)$/i
+          @errors[:name] << "Release name is not a valid scene release name" unless @name =~ REGEXP
           @errors[:name] << "Release name is lowercased" if @name.eql?(@name.downcase)
           @errors[:name] << "Release name is uppercased" if @name.eql?(@name.upcase)
           @errors[:name].empty?
